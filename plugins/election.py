ffc69de5cfb5edf7f94601bc4b2ba69860f8516a
Taven <8206808+daegontaven@users.noreply.github.com>
2022-06-19 17:08:37 +0530

    Add judge election system

--- plugins/election.py
+++ plugins/election.py
@@ -20,15 +20,19 @@ class Election(GroupCog, name="election"):
 
         self.pm_election = False
         self.mp_election = False
+        self.jd_election = False
 
         self.pm_election_candidates: Dict[Member, Candidate] = {}
         self.mp_election_candidates: Dict[Member, Candidate] = {}
+        self.jd_election_candidates: Dict[Member, Candidate] = {}
 
         self.pm_election_ballots: List[Ballot] = []
         self.mp_election_ballots: List[Ballot] = []
+        self.jd_election_ballots: List[Ballot] = []
 
         self.pm_election_roster: List[Member] = []
         self.mp_election_roster: List[Member] = []
+        self.jd_election_roster: List[Member] = []
 
         super().__init__()
 
@@ -40,9 +44,9 @@ class Election(GroupCog, name="election"):
     async def start(
         self,
         interaction: Interaction,
-        kind: Literal["Prime Minister", "House of Commons"],
+        kind: Literal["Prime Minister", "House of Commons", "Judicial Nomination"],
     ) -> None:
-        if self.pm_election or self.mp_election:
+        if self.pm_election or self.mp_election or self.jd_election:
             await update(
                 interaction,
                 embed=Embed(
@@ -58,6 +62,8 @@ class Election(GroupCog, name="election"):
             self.pm_election = True
         elif kind == "House of Commons":
             self.mp_election = True
+        elif kind == "Judicial Nomination":
+            self.jd_election = True
 
         # Send Confirmation Message
         if self.pm_election:
@@ -82,6 +88,17 @@ class Election(GroupCog, name="election"):
             )
             return
 
+        if self.jd_election:
+            await update(
+                interaction,
+                embed=Embed(
+                    title="Election Running",
+                    description="Judges can now be elected.",
+                    color=0x2ECC71,
+                ),
+            )
+            return
+
     @app_commands.command(
         name="vote",
         description="Vote for a candidate.",
@@ -101,6 +118,20 @@ class Election(GroupCog, name="election"):
                     embed=Embed(
                         title="Already Voted",
                         description="You cannot cast your vote twice.",
+                        color=0xE74C3C,
+                    ),
+                    ephemeral=True,
+                )
+                return
+
+            jd = discord.utils.get(interaction.guild.roles, name="Judge")
+            jd = jd.members[0]
+            if jd in [first, second, third]:
+                await update(
+                    interaction,
+                    embed=Embed(
+                        title="Already Elected",
+                        description=f"{jd.mention} is already a Judge.",
                         color=0xE74C3C,
                     ),
                     ephemeral=True,
@@ -194,6 +225,105 @@ class Election(GroupCog, name="election"):
                     )
                 )
                 self.mp_election_roster.append(interaction.user)
+        elif self.jd_election:
+            citizen = discord.utils.get(interaction.guild.roles, name="Citizen")
+            members = [first, second, third]
+            for member in members:
+                if member:
+                    if citizen not in member.roles:
+                        await update(
+                            interaction,
+                            embed=Embed(
+                                title="Invalid Candidate",
+                                description="You can only cast votes for citizens.",
+                                color=0xE74C3C,
+                            ),
+                            ephemeral=True,
+                        )
+                        return
+
+            pm = discord.utils.get(interaction.guild.roles, name="Prime Minister")
+            mp = discord.utils.get(interaction.guild.roles, name="Minister")
+            if pm not in interaction.user.roles or mp not in interaction.user.roles:
+                await update(
+                    interaction,
+                    embed=Embed(
+                        title="Unauthorized",
+                        description=f"Only members of the House of Commons can elect judges..",
+                        color=0xE74C3C,
+                    ),
+                    ephemeral=True,
+                )
+                return
+
+            pm = pm.members[0]
+            if pm in [first, second, third]:
+                await update(
+                    interaction,
+                    embed=Embed(
+                        title="Already Elected",
+                        description=f"{pm.mention} is already Prime Minister.",
+                        color=0xE74C3C,
+                    ),
+                    ephemeral=True,
+                )
+                return
+
+            mp = mp.members[0]
+            if mp in [first, second, third]:
+                await update(
+                    interaction,
+                    embed=Embed(
+                        title="Already Elected",
+                        description=f"{mp.mention} is already a Minister.",
+                        color=0xE74C3C,
+                    ),
+                    ephemeral=True,
+                )
+                return
+
+            if interaction.user in self.jd_election_roster:
+                await update(
+                    interaction,
+                    embed=Embed(
+                        title="Already Voted",
+                        description="You cannot cast your vote twice.",
+                        color=0xE74C3C,
+                    ),
+                    ephemeral=True,
+                )
+                return
+
+            if first and second and third:
+                self.jd_election_ballots.append(
+                    Ballot(
+                        ranked_candidates=[
+                            self.jd_election_candidates[first],
+                            self.jd_election_candidates[second],
+                            self.jd_election_candidates[third],
+                        ]
+                    )
+                )
+                self.jd_election_roster.append(interaction.user)
+            elif first and second and not third:
+                self.jd_election_ballots.append(
+                    Ballot(
+                        ranked_candidates=[
+                            self.jd_election_candidates[first],
+                            self.jd_election_candidates[second],
+                        ]
+                    )
+                )
+                self.jd_election_roster.append(interaction.user)
+            elif first and not second and not third:
+                self.jd_election_ballots.append(
+                    Ballot(
+                        ranked_candidates=[
+                            self.jd_election_candidates[first],
+                        ]
+                    )
+                )
+                self.jd_election_roster.append(interaction.user)
         else:
             await update(
                 interaction,
@@ -224,24 +354,25 @@ class Election(GroupCog, name="election"):
     async def stand(self, interaction: Interaction) -> None:
         member = interaction.user
 
-        pm = discord.utils.get(interaction.guild.roles, name="Prime Minister")
-        pm = pm.members[0]
-        if pm == member:
-            await update(
-                interaction,
-                embed=Embed(
-                    title="Already Elected",
-                    description=f"{pm.mention} is already Prime Minister.",
-                    color=0xE74C3C,
-                ),
-                ephemeral=True,
-            )
-            return
-
         if self.pm_election:
             self.pm_election_candidates[member] = Candidate(name=f"{member.id}")
         elif self.mp_election:
             self.mp_election_candidates[member] = Candidate(name=f"{member.id}")
+        elif self.jd_election:
+            citizen = discord.utils.get(interaction.user.roles, name="Citizen")
+            if citizen in member.roles:
+                self.jd_election_candidates[member] = Candidate(name=f"{member.id}")
+            else:
+                await update(
+                    interaction,
+                    embed=Embed(
+                        title="Unauthorized",
+                        description="Only citizens can stand for election to be a judge.",
+                        color=0xE74C3C,
+                    ),
+                    ephemeral=True,
+                )
+                return
         else:
             await update(
                 interaction,
@@ -349,6 +480,46 @@ class Election(GroupCog, name="election"):
                 ),
                 ephemeral=True,
             )
+        elif self.jd_election:
+            self.jd_election = False
+
+            result = single_transferable_vote(
+                candidates=list(self.jd_election_candidates.values()),
+                ballots=self.jd_election_ballots,
+                number_of_seats=min([50, len(interaction.guild.members) * 0.01]),
+            )
+
+            jd = discord.utils.get(interaction.guild.roles, name="Judge")
+            for member in jd.members:
+                await member.remove_roles(jd, reason="Removed during election.")
+            winners = result.get_winners()
+
+            election_feed = discord.utils.get(
+                interaction.guild.channels, name="election-feed"
+            )
+            for winner in winners:
+                winner = interaction.guild.get_member(int(winner.name))
+                embed = Embed(
+                    title="Minister Elected",
+                    description=f"{winner.mention} is now a Minister of Open Debates ",
+                    colour=0xEC6A5C,
+                )
+                await winner.add_roles(jd, reason="Won election.")
+                await election_feed.send(embed=embed)
+
+            self.jd_election_ballots = []
+            self.jd_election_candidates = {}
+            self.jd_election_roster = []
+
+            await update(
+                interaction,
+                embed=Embed(
+                    title="Election Ended",
+                    description="Judges have been elected.",
+                    color=0x2ECC71,
+                ),
+                ephemeral=True,
+            )
         else:
             await update(
                 interaction,
@@ -362,7 +533,112 @@ class Election(GroupCog, name="election"):
             return
 
 
+class Citizenship(GroupCog, name="citizenship"):
+    def __init__(self, bot: ArgusClient) -> None:
+        self.bot = bot
+
+    @app_commands.command(
+        name="grant",
+        description="Grant citizenship to a Member.",
+    )
+    @app_commands.checks.has_any_role("The Crown", "Chancellor", "Judge")
+    async def grant(self, interaction: Interaction, member: Member) -> None:
+        if "Member" not in [role.name for role in member.roles]:
+            await update(
+                interaction,
+                embed=Embed(
+                    title="Unauthorized",
+                    description="You can only promote Members. Please check if they have that role.",
+                    color=0xE74C3C,
+                ),
+                ephemeral=True,
+            )
+            return
+
+        if "Citizen" in [role.name for role in member.roles]:
+            await update(
+                interaction,
+                embed=Embed(
+                    title="Unauthorized",
+                    description="You can only promote Members.",
+                    color=0xE74C3C,
+                ),
+                ephemeral=True,
+            )
+            return
+
+        if "Judge" in [role.name for role in member.roles]:
+            await update(
+                interaction,
+                embed=Embed(
+                    title="Unauthorized",
+                    description="You can only promote Members.",
+                    color=0xE74C3C,
+                ),
+                ephemeral=True,
+            )
+            return
+
+        citizen = discord.utils.get(interaction.guild.roles, name="Citizen")
+        member_role = discord.utils.get(interaction.guild.roles, name="Member")
+        await member.add_roles(citizen)
+        await member.remove_roles(member_role)
+
+        await update(
+            interaction,
+            embed=Embed(
+                title="Citizenship Granted",
+                description=f"{member.mention} is now a citizen.",
+                color=0x2ECC71,
+            ),
+            ephemeral=True,
+        )
+
+
+class Membership(GroupCog, name="membership"):
+    def __init__(self, bot: ArgusClient) -> None:
+        self.bot = bot
+
+    @app_commands.command(
+        name="grant",
+        description="Grant membership to a new user.",
+    )
+    @app_commands.checks.has_any_role("The Crown", "Chancellor", "Liege")
+    async def grant(self, interaction: Interaction, member: Member) -> None:
+        role_names = [role.name for role in member.roles]
+        if "Member" in role_names or "Citizen" in role_names or "Judge" in role_names:
+            await update(
+                interaction,
+                embed=Embed(
+                    title="Unauthorized",
+                    description="You can only promote regular users.",
+                    color=0xE74C3C,
+                ),
+                ephemeral=True,
+            )
+            return
+
+        member_role = discord.utils.get(interaction.guild.roles, name="Member")
+        await member.add_roles(member_role)
+
+        await update(
+            interaction,
+            embed=Embed(
+                title="Citizenship Granted",
+                description=f"{member.mention} is now a Member.",
+                color=0x2ECC71,
+            ),
+            ephemeral=True,
+        )
+
+
 async def setup(bot: ArgusClient) -> None:
     await bot.add_cog(
         Election(bot), guilds=[discord.Object(id=bot.config["global"]["guild_id"])]
+    )
+    await bot.add_cog(
+        Citizenship(bot), guilds=[discord.Object(id=bot.config["global"]["guild_id"])]
+    )
+    await bot.add_cog(
+        Membership(bot), guilds=[discord.Object(id=bot.config["global"]["guild_id"])]
     )
