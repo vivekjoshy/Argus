a3abffaec37e34681fb6f0bb0f08cd339dc26ae0
Taven <8206808+daegontaven@users.noreply.github.com>
2022-06-21 22:57:04 +0530

    Remove limiter

--- argus/app.py
+++ argus/app.py
@@ -1,8 +1,6 @@
 import sentry_sdk
 import structlog_sentry_logger
 from odmantic import AIOEngine
-from slowapi import Limiter
-from slowapi.util import get_remote_address
 
 from argus.client import ArgusClient
 from argus.config import config
@@ -11,9 +9,6 @@ from argus.db.mongo import MongoClient
 # Logging Config
 sentry_sdk.init(config["bot"]["sentry"], traces_sample_rate=1.0)
 logger = structlog_sentry_logger.get_logger()
-
-# Limiter
-limiter = Limiter(key_func=get_remote_address, default_limits=["5/second"])
 
 # Setup Database
 db = MongoClient(config=config)
